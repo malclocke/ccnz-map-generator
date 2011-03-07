2cb340887cf4fccf56efacf64ec6e895ecd1f3d0
Malcolm Locke <malc@wholemeal.co.nz>
2011-03-07 19:57:21 +1300

    Add initial generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapgen::Application.config.secret_token = '3bc87e3ce2be84e30c90e55e5feaf9f5f863163448e783dd945d9e44d34edadbfcb432d05d4b5bf93a97a8b40ab6ee01a2eaaaa697ea03229d99c25e46144aac'
